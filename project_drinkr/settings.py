4ed08abdbb60b93feea1fbb1d1092e515d7f78d0
John-McPherson <john-mac1989@hotmail.co.uk>
2022-02-28 12:05:52 +0000

    fix typo in allowed hosts

--- project_drinkr/settings.py
+++ project_drinkr/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mixr_cocktail.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['mixr-cocktail.herokuapp.com','localhost']
 
 
 # Application definition
