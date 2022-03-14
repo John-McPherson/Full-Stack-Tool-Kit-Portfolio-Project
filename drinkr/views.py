8bdfd9b227e1e40ad7b805edc72be0f5f3f44321
John-McPherson <john-mac1989@hotmail.co.uk>
2022-03-14 13:47:41 +0000

    add view for home page and set up urls add index.html

--- drinkr/views.py
+++ drinkr/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# from django.views import generic
+
+def home_page(request):
+    return render(request, '../templates/index.html')
