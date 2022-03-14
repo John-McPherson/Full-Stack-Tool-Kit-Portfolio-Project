2ce36d58a9c4e2ae296f27b986ccfbd611164f85
John-McPherson <john-mac1989@hotmail.co.uk>
2022-03-14 10:29:06 +0000

    add UserData class to models.py

--- drinkr/apps.py
+++ drinkr/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DrinkrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'drinkr'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "drinkr"
--- drinkr/models.py
+++ drinkr/models.py
@@ -3,51 +3,80 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-STATUS = ((0,'Awaiting Approval'),(1, 'Approved'))
-INGREDIENT_TYPES = ((0,'Base'),(1, 'Modifier'))
-DRINK_TYPES = ((0,'Up'),(1, 'On The Rocks'), (2,'Long'))
+STATUS = ((0, "Awaiting Approval"), (1, "Approved"))
+INGREDIENT_TYPES = ((0, "Base"), (1, "Modifier"))
+DRINK_TYPES = ((0, "Up"), (1, "On The Rocks"), (2, "Long"))
+
 
 class Ingredients(models.Model):
-    '''
+    """
     A class to represent cocktail ingredients
-    ....
+    ----------
     Attributes
     ----------
     ingredient_name: str
         The name of the ingredient
-    ingredient_type: str 
+    ingredient_type: str
         The type of ingredient (if it is a modifier or a base ingredient)
-    '''
-    ingredient_name = models.CharField(max=200,unique=True)
+    """
+
+    ingredient_name = models.CharField(max=200, unique=True)
     ingredient_type = models.IntegerField(choices=INGREDIENT_TYPES, default=0)
 
     def __str__(self):
         return self.ingredient_name
 
+
 class Recipes(models.Model):
-    '''
+    """
     A class to represent cocktail recipes
-     ....
+    ----------
     Attributes
     ----------
     recipe_name: str
     drink_type: str
-    recipe_steps: list 
+    recipe_steps: list
     ingredients_list: list
     modifiers: list
     author: str
-    approved: boolean 
-    '''
-    recipe_name = models.CharField(max=200,unique=True)
+    approved: boolean
+    """
+
+    recipe_name = models.CharField(max=200, unique=True)
     drink_type = models.IntegerField(choices=DRINK_TYPES, default=0)
     recipe_steps = models.TextField()
     ingredients_list = models.TextField()
     modifiers = models.TextField()
     ingredients = models.TextField()
-    author = models.ForeignKey(USER, on_delete=models.CASCADE,related_name='recipie')
-    approved =  models.IntegerField(choices=STATUS, default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipie")
+    approved = models.IntegerField(choices=STATUS, default=0)
 
-     def __str__(self):
+    def __str__(self):
         return self.recipe_name
 
 
+class UserData(models.Model):
+    """
+    A class to store user data away from senstive login infomation
+    ----------
+    Attributes
+    ----------
+    user_name: str
+    user_ingredients: list
+    user_modifers: list
+    user_drinks: list
+    user_favs: list
+    user_dislikes: list
+    """
+
+    user_name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipie"
+    )
+    user_ingredients = models.TextField()
+    user_modifers = models.TextField()
+    user_drinks = models.TextField()
+    user_favs = models.TextField()
+    user_dislikes = models.TextField()
+
+    def __str__(self):
+        return self.user_name
