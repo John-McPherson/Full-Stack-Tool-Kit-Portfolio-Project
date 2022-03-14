b840e7b157be6ff0ede1fd80ef3edcc43222d5b8
John-McPherson <john-mac1989@hotmail.co.uk>
2022-03-14 10:18:08 +0000

    add Recipes class to models.py

--- drinkr/models.py
+++ drinkr/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# STATUS = ((0,'Awaiting Approval'),(1, 'Approved'))
+STATUS = ((0,'Awaiting Approval'),(1, 'Approved'))
 INGREDIENT_TYPES = ((0,'Base'),(1, 'Modifier'))
+DRINK_TYPES = ((0,'Up'),(1, 'On The Rocks'), (2,'Long'))
 
 class Ingredients(models.Model):
     '''
@@ -22,3 +23,31 @@ class Ingredients(models.Model):
 
     def __str__(self):
         return self.ingredient_name
+
+class Recipes(models.Model):
+    '''
+    A class to represent cocktail recipes
+     ....
+    Attributes
+    ----------
+    recipe_name: str
+    drink_type: str
+    recipe_steps: list 
+    ingredients_list: list
+    modifiers: list
+    author: str
+    approved: boolean 
+    '''
+    recipe_name = models.CharField(max=200,unique=True)
+    drink_type = models.IntegerField(choices=DRINK_TYPES, default=0)
+    recipe_steps = models.TextField()
+    ingredients_list = models.TextField()
+    modifiers = models.TextField()
+    ingredients = models.TextField()
+    author = models.ForeignKey(USER, on_delete=models.CASCADE,related_name='recipie')
+    approved =  models.IntegerField(choices=STATUS, default=0)
+
+     def __str__(self):
+        return self.recipe_name
+
+
