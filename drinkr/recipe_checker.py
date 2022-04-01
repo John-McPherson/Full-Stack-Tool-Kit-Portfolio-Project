6cbebb981d88d241129da0cf0f30f4ae15b87e33
John-McPherson <john-mac1989@hotmail.co.uk>
2022-04-01 13:05:33 +0000

    add code to randomly get a recipe

--- drinkr/recipe_checker.py
+++ drinkr/recipe_checker.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Ingredient, Recipe, UserData
+from random import randint
 
 
 def check_ingredients(user,update_type):
@@ -10,11 +11,13 @@ def check_ingredients(user,update_type):
     
     # gets data from the database and prepares and converts it unto python lists
 
+    user_ingredients = []
+
     if update_type == "base":
-        user_ingredients = user.user_ingredients
+        user_ingredients.extend(user.user_ingredients)
         user_ingredients.extend(user.user_modifers.replace('[','').replace("'","").replace(']','').split(", "))
     else: 
-        user_ingredients = user.user_modifers
+        user_ingredients.extend(user.user_modifers)
         user_ingredients.extend(user.user_ingredients.replace('[','').replace("'","").replace(']','').split(", "))
 
     recipes = Recipe.objects.filter(approved=1).values_list()
@@ -39,3 +42,20 @@ def check_ingredients(user,update_type):
     # returns the final data to be used in the database
     return user_recipes
 
+def get_random_recipe(user):
+    """
+    pulls a random recipe from the users drinks section
+    """
+    # get_random_index()
+    recipe = Recipe.objects.filter(recipe_name=user.user_drinks[get_random_index(len(user.user_drinks))])
+    # print(f'recipes {recipe} type {type(recipe)}')
+    return recipe
+
+def get_random_index(index):
+    """
+    gets a random number to be used as an index to get a random drink recipe.
+
+    """
+    random = randint(0,index-1)
+    print(random)
+    return(random)
