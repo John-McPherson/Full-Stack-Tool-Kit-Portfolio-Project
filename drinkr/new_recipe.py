80bf74be087c3562d2865628ce8dd73147169ff3
John-McPherson <john-mac1989@hotmail.co.uk>
2022-04-03 14:48:17 +0000

    add code to check if ing is a modifer or base

--- drinkr/new_recipe.py
+++ drinkr/new_recipe.py
@@ -1,3 +1,6 @@
+
+from .models import Ingredient, Recipe, UserData
+
 def recipe_steps(recipe):
     """
     Gets the steps from from and formats them into usable data
@@ -14,11 +17,35 @@ def ingredient_list(ingredients,measurement, volume):
     """
     Converts form data ingredient, measurement, and volume into usable data
     """
-    list = []
+    step_list = []
     y = 0 
     for ingredient in ingredients:
         if ingredient:
-            line = f'{volume[y]} {measurement[y]} of {ingredient}'
+            line = f'{volume[y]} {measurement[y]} of {ingredient.title()}'
             y+=1
-            list.append(line)
-    return list
+            step_list.append(line)
+    return step_list
+
+def modifer_or_ingredient_list(ingredients, type):
+    """
+    checks to see if an ingrdient is a modifier or an ingredient
+    """
+    ing_list = []
+    new_ing_list = []
+    for ingredient in ingredients:
+        if ingredient:
+            if type == "modifier":
+                ing = Ingredient.objects.filter(ingredient_type=1).filter(ingredient_name=ingredient).values_list()
+            else: 
+                ing = Ingredient.objects.filter(ingredient_type=0).filter(ingredient_name=ingredient).values_list()
+            if ing:
+                ing_list.append(ingredient)
+            else:
+                new_ing_list.append(ingredient)
+    if type == 'new':
+        return new_ing_list
+    else: 
+        return ing_list
+    # print(queryset)
+
+
