e8e90e8e5beef91f4ae2db3ce49f1cf8c14ee524
John-McPherson <john-mac1989@hotmail.co.uk>
2022-04-03 16:22:40 +0000

    add form handler to submit recipe form

--- drinkr/views.py
+++ drinkr/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic, View
 from .models import Ingredient, Recipe, UserData
 from .recipe_checker import check_ingredients, get_random_recipe
-from .forms import NewRecipeForm
+from .new_recipe import recipe_steps, ingredient_list, modifer_or_ingredient_list
 
 
 
@@ -108,10 +108,40 @@ class SubmitRecipe(View):
 
         return render(
             request,'submit_recipe.html',
-            {
-                 'recipe_form' : NewRecipeForm(),
-            }
+           
+            )
+
+    def post(self,request, *args, **kwargs):
+        model = Recipe
+        if Recipe.objects.filter(recipe_name=request.POST.get('drink-name').title()).exists():
+            title = request.POST.get('drink-name').title()
+            does_recipe_exist = True
+        else:
+            recipe = Recipe()
+            does_recipe_exist = False
+
+            ingredients = modifer_or_ingredient_list(request.POST.getlist('ingredient'), 'ingredient')
+            measurement = request.POST.getlist('measurement')
+            volume = request.POST.getlist('volume')
+            title = request.POST.get('drink-name').title()
+
+            recipe.recipe_name = title
+            recipe.drink_type = 0
+            recipe.approved = 0
+            recipe.author = request.user
+            recipe.ingredients = modifer_or_ingredient_list(request.POST.getlist('ingredient'), 'ingredient')
+            recipe.modifiers = modifer_or_ingredient_list(request.POST.getlist('ingredient'), 'modifier')
+            recipe.ingredients_list = ingredient_list(ingredients, measurement, volume)
+            recipe.recipe_steps = recipe_steps(request.POST.getlist('step'))
+            recipe.new_ingredients = modifer_or_ingredient_list(request.POST.getlist('ingredient'), 'new')
+            recipe.save()
+
+
+        return render(
+            request,'form_submit.html',{
+                'recipe_name': title , 
+                'exists': does_recipe_exist
+            }  
             )
 
 
-    # post(self,request, *args, **kwargs):
