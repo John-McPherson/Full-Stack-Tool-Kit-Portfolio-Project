53246aa1aefa45a2c13066984da62d2ff53b5e84
John-McPherson <john-mac1989@hotmail.co.uk>
2022-04-05 21:39:13 +0000

    add view to update liked and disliked drinks

--- drinkr/views.py
+++ drinkr/views.py
@@ -3,8 +3,9 @@ from django.views import generic, View
 from .models import Ingredient, Recipe, UserData
 from .recipe_checker import check_ingredients, get_random_recipe
 from .new_recipe import recipe_steps, ingredient_list, modifer_or_ingredient_list
+from . likes_dislikes import likes
 
-
+current_recipe = None
 
 def home_page(request):
     return render(request, '../templates/index.html')
@@ -80,25 +81,68 @@ class ModifierList(View):
         user.user_modifers = updated_user_ingredients
         user.user_drinks = check_ingredients(user, "modifiers")
         user.save()
+        global current_recipe 
+        current_recipe = get_random_recipe(user)
+
+
 
         return render(
-            request,'recipe.html',
+            request,'confirm_recipe.html',
             {
-                 'recipe' : get_random_recipe(user)
+                 'recipe' : current_recipe
             }
             )
 
 
 
-class ConfirmRecipe(generic.ListView):
+class ConfirmRecipe(View):
+    
     model = Recipe
     queryset = Recipe.objects.filter(approved=1)
     template_name = 'confirm_recipe.html'
 
-class DisplayRecipe(generic.ListView):
-    model = Recipe
-    queryset = Recipe.objects.filter(approved=1)
-    template_name = 'recipe.html'
+class DisplayRecipe(View):
+
+    def get(self,request):
+    
+
+        return render(
+            request,'recipe.html',
+            {
+                 'recipe' : current_recipe
+            }
+            )
+    
+    def post(self,request, *args, **kwargs):
+
+        user = UserData.objects.get(user_name=request.user)
+        if request.POST.getlist('liked')[0] == 'like': 
+            user.user_favs = likes(request.POST.getlist('drink_name')[0], user.user_favs)
+            user.save()
+            # print(request.POST.getlist('drink_name')[0])
+            # print("liked")
+        else: 
+            user.user_dislikes = likes(request.POST.getlist('drink_name')[0], user.user_dislikes)
+            user.save()
+        return render(
+            request,'recipe.html',
+            {
+                 'recipe' : current_recipe
+            }
+            )
+
+        # model = Ingredient
+        # queryset = Ingredient.objects.filter(ingredient_type=0)
+        # ingredient_list = queryset
+        # user_ingredients = UserData.objects.filter(user_name=request.user).values_list('user_ingredients')
+
+        # updated_user_ingredients = request.POST.getlist('ingredient')
+
+        # user = UserData.objects.get(user_name=request.user)
+        # user.user_ingredients = updated_user_ingredients
+        # user.user_drinks = check_ingredients(user, 'base')
+        # user.save()
+    
 
 
 class SubmitRecipe(View):
