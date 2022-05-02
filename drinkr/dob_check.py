3aea8bf3987fb8814e5fd7f06147b327cdb2a588
John-McPherson <john-mac1989@hotmail.co.uk>
2022-05-02 19:08:15 +0000

    fix dob_check returning error when using db data

--- drinkr/dob_check.py
+++ drinkr/dob_check.py
@@ -1,12 +1,16 @@
-from datetime import date
+from datetime import date, datetime
 
 def dob_check(userdob):
-# """
-# returns true if the user is older than 18
-# """
+    """
+    returns true if the user is older than 18
+    """
     now = date.today()
+    if isinstance(userdob,str):
+
+        userdob = datetime.strptime(userdob.replace("-"," "), '%Y %m %d')
+
     age = now.year - userdob.year - ((now.month, now.day) < (userdob.month, userdob.day))
-    print(age)
+
     if age > 18:
         return True
     else:
