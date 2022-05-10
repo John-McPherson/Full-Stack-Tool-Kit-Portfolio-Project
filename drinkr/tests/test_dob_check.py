4db361a2147c70b6637e81a8ce76db448c3de5f2
John-McPherson <john-mac1989@hotmail.co.uk>
2022-05-10 22:10:15 +0000

    add test suite for dob_check

---
+++ drinkr/tests/test_dob_check.py
@@ -0,0 +1,20 @@
+from ..dob_check import dob_check
+from django.test import TestCase
+import datetime
+
+# Create your tests here.
+class TestDob(TestCase):
+
+
+    def test_string_returns_true_if_user_is_18_plus(self):
+        self.assertTrue(dob_check("1989-12-15"))
+
+    def test_string_returns_false_if_user_under_18(self):
+        self.assertFalse(dob_check("2020-12-15"))
+    
+    def test_datetime_returns_false_if_user_under_18(self):
+        self.assertFalse(dob_check(datetime.datetime.now()))
+
+    def test_datetime_returns_true_if_user_over_18(self):
+        self.assertTrue(dob_check(datetime.datetime(2000, 5, 17)))
+    
