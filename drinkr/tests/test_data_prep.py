a2de5ef29e6ee1016110baaf2ff1e7997f16612a
John-McPherson <john-mac1989@hotmail.co.uk>
2022-05-21 10:17:05 +0000

    add test data_prep and modify  so that it passes

--- drinkr/tests/test_data_prep.py
+++ drinkr/tests/test_data_prep.py
@@ -4,3 +4,5 @@ from ..data_prep import data_prep
 class TestData(TestCase):
     def test_if_passed_plain_text_returns_unmodified(self):
         self.assertEqual(data_prep("test"), "test")
+    def test_if_passed_a_different_plain_text_rtrns_unmodified(self):
+        self.assertEqual(data_prep("test two"), "test two")
