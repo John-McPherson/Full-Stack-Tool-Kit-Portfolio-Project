e8c1b8951ba78580faf8bc297138589e5a5869b0
John-McPherson <john-mac1989@hotmail.co.uk>
2022-06-12 11:31:43 +0000

    temp add line to allow iamresponsive to work

--- project_drinkr/settings.py
+++ project_drinkr/settings.py
@@ -67,7 +67,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+    X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'project_drinkr.urls'
 
